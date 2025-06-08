9890c650f885e3c07b751c1e93143060b95610c4
shinra-electric <50119606+shinra-electric@users.noreply.github.com>
2025-06-08 12:07:08 +0100

    Keep System Services

--- src/frontend/swiftui/MenuCommands.swift
+++ src/frontend/swiftui/MenuCommands.swift
@@ -6,21 +6,20 @@ struct MenuCommands: Commands {
         // Adds a new command to the File menu
         CommandGroup(after: .newItem) {
             Button("Add to Library") {
-
+                // TODO: Implement
             }
             .keyboardShortcut(KeyEquivalent("o"), modifiers: .command)
             
             Button("Boot from File") {
-
+                // TODO: Implement
             }
             .keyboardShortcut(KeyEquivalent("b"), modifiers: .command)
         }
 
-
         // This removes/replaces menu items
         CommandGroup(replacing: CommandGroupPlacement.help) {}
         CommandGroup(replacing: CommandGroupPlacement.pasteboard) {}
-        CommandGroup(replacing: CommandGroupPlacement.systemServices) {}
         CommandGroup(replacing: CommandGroupPlacement.undoRedo) {}
+//        CommandGroup(replacing: CommandGroupPlacement.systemServices) {}
     }
 }
