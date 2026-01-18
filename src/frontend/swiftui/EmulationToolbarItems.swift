82a420f144ff595493cf5ee494211b84ab4511f0
shinra-electric <50119606+shinra-electric@users.noreply.github.com>
2026-01-18 18:20:37 +0000

    Add modes to toolbar

--- src/frontend/swiftui/EmulationToolbarItems.swift
+++ src/frontend/swiftui/EmulationToolbarItems.swift
@@ -4,6 +4,7 @@ struct EmulationToolbarItems: ToolbarContent {
     @EnvironmentObject var globalState: GlobalState
 
     @State private var isRunning: Bool = false
+    @State private var isHandheldMode = false
 
     var body: some ToolbarContent {
         #if os(macOS)
@@ -26,6 +27,28 @@ struct EmulationToolbarItems: ToolbarContent {
                 }
                 .onAppear {
                     isRunning = globalState.emulationContext!.isRunning()
+                }
+            }
+            ToolbarItemGroup(placement: .principal) {
+                Button("Console Mode", systemImage: "inset.filled.tv") {
+                    hydraConfigGetHandheldMode().pointee = false
+                    hydraConfigSerialize()
+                    
+                    guard let emulationContext = globalState.emulationContext else { return }
+                    emulationContext.notifyOperationModeChanged()
+                }
+                .disabled(!isHandheldMode)
+                
+                Button("Handheld Mode", systemImage: "formfitting.gamecontroller.fill") {
+                    hydraConfigGetHandheldMode().pointee = true
+                    hydraConfigSerialize()
+                    
+                    guard let emulationContext = globalState.emulationContext else { return }
+                    emulationContext.notifyOperationModeChanged()
+                }
+                .disabled(isHandheldMode)
+                .onAppear {
+                    isHandheldMode = hydraConfigGetHandheldMode().pointee
                 }
             }
         #else
