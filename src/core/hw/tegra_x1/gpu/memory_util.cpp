38b0ba23c755dbde58fa42bcb9565da218cd21f8
Samuel Žúbor <samuliak77@gmail.com>
2026-05-18 11:30:20 +0200

    gpu: cleanup layout conversions

--- src/core/hw/tegra_x1/gpu/memory_util.cpp
+++ src/core/hw/tegra_x1/gpu/memory_util.cpp
@@ -4,49 +4,57 @@ namespace hydra::hw::tegra_x1::gpu {
 
 namespace {
 
+// block_height_log2 is in GOBs
 template <bool to_block_linear>
 void Convert(u32 stride, u32 rows, u32 block_height_log2, u8* block_linear,
              u8* linear) {
-    const auto block_height_gobs = 1u << block_height_log2;
-    const auto block_height_px = 8u << block_height_log2;
+    constexpr u32 GOB_WIDTH_LOG2 = 6;
+    constexpr u32 GOB_HEIGHT_LOG2 = 3;
+    constexpr u32 GOB_WIDTH = 1 << GOB_WIDTH_LOG2;
+    constexpr u32 GOB_HEIGHT = 1 << GOB_HEIGHT_LOG2;
 
-    const auto horizontal_blocks = stride >> 6;
-    const auto vertical_blocks =
-        (rows + block_height_px - 1) >> (3 + block_height_log2);
+    const u32 block_height_gobs = 1 << block_height_log2;
+    const u32 block_height_px = GOB_HEIGHT << block_height_log2;
+
+    const u32 horizontal_blocks =
+        stride >> GOB_WIDTH_LOG2; // GOBs = blocks (horizontally)
+    const u32 vertical_blocks =
+        align(rows, block_height_px) >> (GOB_HEIGHT_LOG2 + block_height_log2);
 
     // Clear the output buffer first
     // TODO: is this necessary?
     // memset(out_data, 0, stride * height);
 
-    constexpr usize BLOCK_SIZE = 32;
+    constexpr usize GOB_SIZE = 512;
 
     for (u32 block_y = 0; block_y < vertical_blocks; block_y++) {
         for (u32 block_x = 0; block_x < horizontal_blocks; block_x++) {
             for (u32 gob_y = 0; gob_y < block_height_gobs; gob_y++) {
-                const u32 x = block_x * 64;
-                const u32 y = block_y * block_height_px + gob_y * 8;
-                if (y < rows) {
-                    u8* decoded_gob = linear + y * stride + x;
-                    // Reverse the 16Bx2 swizzling for each GOB
-                    for (u32 i = 0; i < BLOCK_SIZE; i++) {
-                        const u32 local_y = ((i >> 1) & 0x06) | (i & 0x01);
-                        const u32 local_x =
-                            ((i << 3) & 0x10) | ((i << 1) & 0x20);
-
-                        auto linear_data = reinterpret_cast<u128*>(
-                            decoded_gob + local_y * stride + local_x);
-                        auto block_linear_data =
-                            reinterpret_cast<u128*>(block_linear);
-                        if constexpr (to_block_linear)
-                            *block_linear_data = *linear_data;
-                        else
-                            *linear_data = *block_linear_data;
-
-                        block_linear += sizeof(u128);
-                    }
-                } else {
+                const u32 x = block_x * GOB_WIDTH;
+                const u32 y = block_y * block_height_px + gob_y * GOB_HEIGHT;
+                if (y >= rows) {
                     // Skip this GOB if we're past the valid height
-                    block_linear += sizeof(u128) * BLOCK_SIZE;
+                    block_linear += GOB_SIZE;
+                    continue;
+                }
+
+                u8* decoded_gob = linear + y * stride + x;
+
+                // Reverse the 16Bx2 swizzling for each GOB
+                for (u32 i = 0; i < GOB_SIZE / sizeof(u128); i++) {
+                    const u32 local_y = ((i >> 1) & 0x06) | (i & 0x01);
+                    const u32 local_x = ((i << 3) & 0x10) | ((i << 1) & 0x20);
+
+                    auto linear_data = reinterpret_cast<u128*>(
+                        decoded_gob + local_y * stride + local_x);
+                    auto block_linear_data =
+                        reinterpret_cast<u128*>(block_linear);
+                    if constexpr (to_block_linear)
+                        *block_linear_data = *linear_data;
+                    else
+                        *linear_data = *block_linear_data;
+
+                    block_linear += sizeof(u128);
                 }
             }
         }
